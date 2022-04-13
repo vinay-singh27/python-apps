663363427609788c07b4aedc028d10e343f758af
Vinay Singh <vinaysin27@gmail.com>
2022-04-13 20:22:33 +0530

    updated main.py

---
+++ Instant_Dictionary/main.py
@@ -0,0 +1,20 @@
+import justpy as jp
+import inspect
+
+from webapp.about import About
+from webapp.dictionary import Dictionary
+from webapp.home import Home
+from webapp import page
+
+imports = list(globals().values())
+
+for obj in imports:
+    if inspect.isclass(obj):
+        if issubclass(obj, page.Page) and obj is not page.Page:
+            jp.Route(obj.path, obj.serve)
+
+# jp.Route(Home.path, Home.serve)
+# jp.Route(About.path, About.serve)
+# jp.Route(Dictionary.path, Dictionary.serve)
+
+jp.justpy(port=8001)
